5829a60b8fc2fc2d25b89da42aec92f4353d456a
David Giraldo <davidgiraldobueno@gmail.com>
2020-06-01 17:41:19 -0500

    react init

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
 import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    console.log('User List');
+
+    res.json({ Message: 'Hello World2!' });
+});
+
+app.listen(3333);
