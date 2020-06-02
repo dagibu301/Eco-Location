4819cde38918c4f2a2d55ddade08bc89742482c3
David Giraldo <davidgiraldobueno@gmail.com>
2020-06-02 17:51:13 -0500

    basic routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,32 @@ import express from 'express';
 
 const app = express();
 
+const users = [
+    'David',
+    'Eliana',
+    'Wasabina'
+];
+
 app.get('/users', (req, res) => {
     console.log('User List');
 
-    res.json({ Message: 'Hello World2!' });
+    return res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+    console.log('Unique User');
+    const id = Number(req.params.id);
+
+    return res.json(users[id]);
+});
+
+app.post('/users',(req,res) => {
+    const user = {
+        name: 'David',
+        lastname: 'Giraldo'
+    };
+
+    return res.json(user);
 });
 
 app.listen(3333);
