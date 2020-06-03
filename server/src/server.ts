42c50807d740fddfcb0e568f5e20979c153f972a
David Giraldo <davidgiraldobueno@gmail.com>
2020-06-02 21:38:58 -0500

    routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,40 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-const users = [
-    'David',
-    'Eliana',
-    'Wasabina'
-];
-
-app.get('/users', (req, res) => {
-    const search = String(req.query.search);
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-    console.log(filteredUsers);
-
-    return res.json(filteredUsers);
-});
-
-app.get('/users/:id', (req, res) => {
-    console.log('Unique User');
-    const id = Number(req.params.id);
-
-    return res.json(users[id]);
-});
-
-app.post('/users',(req,res) => {
-
-    const data = req.body;
-
-    const user = {
-        name: data.name,
-        lastname: data.lastname
-    }
-
-    return res.json(user);
-});
+app.use(routes);
 
 app.listen(3333);
